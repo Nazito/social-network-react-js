f3f9e0efce3b881aa01a39ed101fbf9cda677b6e
Nazar Tolstoy <nazar.tolstoy@topstack.dev>
2020-04-16 21:36:16 +0300

    Updates

---
+++ precache-manifest.e01dc355badaf7b78335658efd978be7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676e290b88bc21e61ba1c78db931d0de",
+    "url": "/social-network-react-js/index.html"
+  },
+  {
+    "revision": "00679dbaa90250dd11cb",
+    "url": "/social-network-react-js/static/css/3.a227e1b7.chunk.css"
+  },
+  {
+    "revision": "4329b6f69f92c6616fb1",
+    "url": "/social-network-react-js/static/css/4.b90bc758.chunk.css"
+  },
+  {
+    "revision": "204f7cb40ffd71f1ae49",
+    "url": "/social-network-react-js/static/css/main.62ca2fd5.chunk.css"
+  },
+  {
+    "revision": "8135f5f80525608ceed2",
+    "url": "/social-network-react-js/static/js/2.453ba7ce.chunk.js"
+  },
+  {
+    "revision": "00679dbaa90250dd11cb",
+    "url": "/social-network-react-js/static/js/3.52e27ea6.chunk.js"
+  },
+  {
+    "revision": "4329b6f69f92c6616fb1",
+    "url": "/social-network-react-js/static/js/4.24a2c144.chunk.js"
+  },
+  {
+    "revision": "204f7cb40ffd71f1ae49",
+    "url": "/social-network-react-js/static/js/main.b5ba8914.chunk.js"
+  },
+  {
+    "revision": "9f96c99a9c9c79118ef0",
+    "url": "/social-network-react-js/static/js/runtime-main.b6fe582b.js"
+  },
+  {
+    "revision": "925d37989fbb7e5286d218c1795c6912",
+    "url": "/social-network-react-js/static/media/computer-icons-user-profile-clip-art-png-favpng-MtANWqcpdGATjaV7qwJpkYcdq.925d3798.jpg"
+  }
+]);
